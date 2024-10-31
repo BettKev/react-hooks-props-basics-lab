ce7852b05edd45e51ce6fd301db44229b307a741
Kevin Bett <prodigy4614@gmail.com>
2024-10-31 22:39:13 +0300

    fixed bug not with naming props

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About({ bio, githubLink, linkedinLink }) {
       <h2>About Me</h2>
       {bio && <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links githubLink={githubLink} linkedinLink={linkedinLink} />
+      <Links github={githubLink} linkedin={linkedinLink} />
     </div>
   );
 }
